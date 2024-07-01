236a9ca8c39ed39b378083ac383a79912a070f23
hexbloom <hexbloom@proton.me>
2024-07-01 17:04:17 -0400

    git files remap and zls json

--- nvim.lua
+++ nvim.lua
@@ -237,6 +237,7 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc =
 vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>t', require('telescope.builtin').builtin, { desc = 'Search Select [T]elescope' })
 vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' })
+vim.keymap.set('n', '<leader>g', require('telescope.builtin').git_files, { desc = 'Search [F]iles' })
 vim.keymap.set('n', '<leader>s', require('telescope.builtin').live_grep, { desc = '[S]earch by Grep' })
 
 -- [[ Configure Treesitter ]]
