786666dcacb967dbaecb7a005802178e7099b795
hexbloom <hexbloom@proton.me>
2024-07-02 16:06:22 -0400

    fix grep files in neovim

--- nvim.lua
+++ nvim.lua
@@ -88,16 +88,9 @@ require('lazy').setup({
     -- Fuzzy Finder (files, lsp, etc)
     {
         'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
+        tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim',
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build = 'make',
-                cond = function()
-                    return vim.fn.executable 'make' == 1
-                end,
-            },
         },
     },
 
